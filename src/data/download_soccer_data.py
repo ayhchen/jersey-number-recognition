6f926453a37984aa57cda4d15e701c54086371a2
Adams Chen <ayhchen96@gmail.com>
2025-01-21 17:19:51 -0800

    refilled empty download_soccer_data.py

---
+++ src/data/download_soccer_data.py
@@ -0,0 +1,3 @@
+from SoccerNet.Downloader import SoccerNetDownloader as SNdl
+mySNdl = SNdl(LocalDirectory="path/to/SoccerNet")
+mySNdl.downloadDataTask(task="jersey-2023", split=["train","test","challenge"])
